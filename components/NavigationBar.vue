d78b8e0369602d23489504d1f1c56dda02f1aff1
William Gvozdjak <w.gvoz@outlook.com>
2022-10-11 21:46:14 -0700

    Make a few more stylistic changes

--- components/NavigationBar.vue
+++ components/NavigationBar.vue
@@ -1,16 +1,18 @@
+<!-- all the fonts used to be font-josefin -->
+
 <template>
     <header class="w-full bg-red">
         <div class="flex my-0 mx-auto py-4 max-w-screen-2xl w-full px-2">
             <nav class="items-center flex flex-1">
                 <div class="flex">
-                    <a href="/" class="px-4 py-2 text-2xl text-slate-900 font-josefin">bookify.</a>
+                    <a href="/" class="px-4 py-2 text-xl">bookify.</a>
                 </div>
                 <div class="flex flex-1 justify-end relative">
-                    <a href="/" class="px-4 py-2 text-2xl text-slate-900 font-josefin">home</a>
-                    <a href="#" class="px-4 py-2 text-2xl text-slate-900 font-josefin">goals</a>
-                    <a href="#" class="px-4 py-2 text-2xl text-slate-900 font-josefin">data</a>
-                    <a href="#" class="px-4 py-2 text-2xl text-slate-900 font-josefin">recommendations</a>
-                    <button href="#" class="px-4 py-2 text-2xl text-slate-900 font-josefin" @click="$emit('settingsClicked')">settings</button>
+                    <a href="/" class="px-4 py-2 text-xl">home</a>
+                    <a href="#" class="px-4 py-2 text-xl">goals</a>
+                    <a href="#" class="px-4 py-2 text-xl">data</a>
+                    <a href="#" class="px-4 py-2 text-xl">recommendations</a>
+                    <button href="#" class="px-4 py-2 text-xl" @click="$emit('settingsClicked')">settings</button>
                 </div>
             </nav>
         </div>
