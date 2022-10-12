f40e23e8d104902a80cb75e68e5ceaa1d12210bd
wgvozdjak <s-gvozdjakw@bsd405.org>
2022-10-12 01:57:19 +0000

    Change settings popup if signed in

--- components/SettingsPopup.vue
+++ components/SettingsPopup.vue
@@ -1,35 +1,43 @@
 <template>
   <div v-if="isVisible" class="flex w-full h-full fixed top-0 left-0 justify-center items-center bg-black bg-opacity-20">
-    <form class="shadow-xl flex bg-solid bg-white border-2 border-solid border-black w-[425px] rounded-3xl p-4 pb-5" @submit.prevent="handleLogin">
-      <div class="flex flex-col gap-3 w-full">
-        <div class="w-full flex flex-row justify-between">
-          <h1 class="font-bold text-xl">bookify settings</h1>
-          <button class="text-xl" @click="closePopup">&#x2715;</button>
-        </div>
-        <!--<p class="description ">Sign in via magic link with your email below</p>-->
-        <div class="flex flex-col gap-1 w-full">
-          <h2>Sign in with magic link</h2>
-          <input
-            class="w-full border-2 border-solid border-gray-300 p-1 rounded-md"
-            type="email"
-            placeholder="Your email"
-            v-model="email"
-          />
-          <div>
+    <div class="shadow-xl flex bg-solid bg-white border-2 border-solid border-black w-[425px] rounded-3xl p-4 pb-5">
+      <form v-if="!user" @submit.prevent="handleLogin" class="w-full">
+        <div class="flex flex-col gap-3 w-full">
+          <div class="w-full flex flex-row justify-between">
+            <h1 class="font-bold text-xl">bookify settings</h1>
+            <button class="text-xl" @click="closePopup">&#x2715;</button>
+          </div>
+          <!--<p class="description ">Sign in via magic link with your email below</p>-->
+          <div class="flex flex-col gap-1 w-full">
+            <h2>Sign in with magic link</h2>
             <input
-              type="submit"
-              class="w-full border-2 border-solid border-gray-300 bg-gray-200 rounded-md p-1 text-left cursor-pointer hover:bg-gray-300 active:bg-gray-400 active:border-gray-400"
-              :value="loading ? 'Loading' : 'Send magic link'"
-              :disabled="loading"
+              class="w-full border-2 border-solid border-gray-300 p-1 rounded-md"
+              type="email"
+              placeholder="Your email"
+              v-model="email"
             />
+            <div>
+              <input
+                type="submit"
+                class="w-full border-2 border-solid border-gray-300 bg-gray-200 rounded-md p-1 text-left cursor-pointer hover:bg-gray-300 active:bg-gray-400 active:border-gray-400"
+                :value="loading ? 'Loading' : 'Send magic link'"
+                :disabled="loading"
+              />
+            </div>
           </div>
         </div>
-      </div>
-    </form>
+      </form>
+
+      <form v-else>
+        you are signed in.
+      </form>
+    </div>
   </div>
 </template>
   
 <script setup>
+  const user = useSupabaseUser();
+
   let isVisible = ref(false);
   function closePopup() {
     isVisible.value = false;
