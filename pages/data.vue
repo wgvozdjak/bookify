24e69dbf52bc4d7a9abfcf59bc8cfc4803f0cf38
William Gvozdjak <70719982+wgvozdjak@users.noreply.github.com>
2022-11-16 19:42:14 +0000

    Add chart.js and a basic usage

--- pages/data.vue
+++ pages/data.vue
@@ -1,13 +1,42 @@
 <template>
-  <div class="flex flex-col">
-    <div class="font-bold">data</div>
-    <div class="flex flex-row">
-      <div class="basis-1/2">
-        <h2>basic statistics</h2>
-      </div>
-      <div class="basis-1/2">
-        <h2>graphs and tables</h2>
+  <div>
+    <div class="flex flex-col">
+      <div class="font-bold">data</div>
+      <div class="flex flex-row">
+        <div class="basis-1/2">
+          <h2>basic statistics</h2>
+        </div>
+        <div class="basis-1/2">
+          <h2>graphs and tables</h2>
+
+          <GChart
+            type="ColumnChart"
+            :data="chartData"
+            :options="chartOptions"
+          />
+        </div>
       </div>
     </div>
   </div>
 </template>
+
+<script setup>
+import { GChart } from "vue-google-charts";
+
+const chartData = ref([
+  ["Year", "Sales"],
+  ["2014", 1000],
+  ["2015", 1170],
+  ["2016", 660],
+  ["2017", 1030],
+]);
+
+const chartOptions = ref({
+  chart: {
+    title: "Company Performance",
+    subtitle: "Sales: 2014-2017",
+  },
+  colors: ["a78bfa"],
+  backgroundColor: "transparent",
+});
+</script>
