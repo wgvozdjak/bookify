b7a9d1e3328aea0f1d29e9b147629853c5388be8
William Gvozdjak <70719982+wgvozdjak@users.noreply.github.com>
2022-11-22 22:04:44 +0000

    Continue working on graphs dropdown component

--- components/data/GraphsDropdown.vue
+++ components/data/GraphsDropdown.vue
@@ -1,11 +1,35 @@
 <template>
-  <div class="w-full rounded-lg bg-white/90 p-[3px]">
+  <div class="w-full rounded-lg bg-white/70 p-[3px] shadow-md">
     <button
-      class="w-full rounded-md p-2 text-left transition hover:bg-gray-200"
+      class="flex w-full items-center justify-between rounded-md p-2 text-left transition hover:bg-gray-200"
+      @click="expanded = !expanded"
     >
       book genre distribution
+
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="h-5 w-5"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+        />
+      </svg>
     </button>
+
+    <TransitionCollapseHeight>
+      <div v-show="expanded"><div class="p-2">this is a test</div></div>
+    </TransitionCollapseHeight>
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+import TransitionCollapseHeight from "../TransitionCollapseHeight.vue";
+
+const expanded = ref(false);
+</script>
