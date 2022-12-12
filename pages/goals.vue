3eec2c74802d570f2ea4f5e6768c8ebeda9e23e2
William Gvozdjak <70719982+wgvozdjak@users.noreply.github.com>
2022-12-12 22:04:07 +0000

    Show existing goals on page load

--- pages/goals.vue
+++ pages/goals.vue
@@ -3,16 +3,67 @@
 
   <button @click="setNewGoal">set new goal</button>
 
-  <div></div>
+  <div>
+    <div v-for="(goal, index) in goals">
+      <h2>Goal #{{ index }}</h2>
+      <p>Start date: {{ goal.start_date }}</p>
+      <p>End date: {{ goal.end_date }}</p>
+      <p>Goal: {{ goal.book_count }}</p>
+      <p>Status: {{ !goal.status ? "in progress" : "finished" }}</p>
+    </div>
+  </div>
 </template>
 
 <script setup>
 import NewGoalPopup from "../components/goals/NewGoalPopup.vue";
+
+let user = useSupabaseUser();
+const supabase = useSupabaseClient();
+
+const goals = ref([]);
+
+let user_loaded = false;
+if (user.value) {
+  getGoals();
+  user_loaded = true;
+}
+supabase.auth.onAuthStateChange((event, session) => {
+  if (!user_loaded) {
+    if (event == "SIGNED_IN") {
+      const timer = setInterval(async () => {
+        user = useSupabaseUser();
+        if (user.value) {
+          clearInterval(timer);
+          getGoals();
+          user_loaded = true;
+        }
+      }, 100);
+    }
+  }
+});
 
 const newGoalPopup = ref(null);
 function setNewGoal() {
   newGoalPopup.value.openPopup();
 }
 
-async function getGoals() {}
+async function getGoals() {
+  const { data, error } = await supabase
+    .from("goals")
+    .select()
+    .eq("id", user.value.id);
+
+  // TODO: i could probably just directly use the data variable in the vue code
+
+  console.log(data);
+  for (let goal of data) {
+    goals.value.push({
+      book_count: goal.book_count,
+      start_date: goal.start_date,
+      finish_date: goal.finish_date,
+      status: goal.status,
+    });
+  }
+  console.log(goals.value);
+}
 </script>
